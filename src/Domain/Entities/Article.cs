ea44977a07bc4f6740b913c0cbeec8bd32a9fe0d
japheth <japhemchirie@gmail.com>
2026-09-02 12:56:03 +0300

    Add Article and Gif entities inheriting from Post (TPT)

---
+++ src/Domain/Entities/Article.cs
@@ -0,0 +1,7 @@
+﻿namespace TeamworkApp.Domain.Entities;
+
+public class Article : Post
+{
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+}
---
+++ src/Domain/Entities/Gif.cs
@@ -0,0 +1,7 @@
+﻿namespace TeamworkApp.Domain.Entities;
+
+public class Gif : Post
+{
+    public string Url { get; set; } = string.Empty;
+    public string Caption { get; set; } = string.Empty;
+}
