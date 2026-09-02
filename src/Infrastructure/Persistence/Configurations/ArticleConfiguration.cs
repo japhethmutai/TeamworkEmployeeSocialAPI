f53c43bb91a7399f7cab3d957f25d3dd1cce572a
japheth <japhemchirie@gmail.com>
2026-09-02 16:06:56 +0300

    Configurations for Article and Gif database tables

---
+++ src/Infrastructure/Persistence/Configurations/ArticleConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TeamworkApp.Domain.Entities;
+
+namespace TeamworkApp.Infrastructure.Persistence.Configurations;
+
+public class ArticleConfiguration : IEntityTypeConfiguration<Article>
+{
+    public void Configure(EntityTypeBuilder<Article> builder)
+    {
+        builder.ToTable("Articles");
+    }
+}
---
+++ src/Infrastructure/Persistence/Configurations/GifConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TeamworkApp.Domain.Entities;
+
+namespace TeamworkApp.Infrastructure.Persistence.Configurations;
+
+public class GifConfiguration : IEntityTypeConfiguration<Gif>
+{
+    public void Configure(EntityTypeBuilder<Gif> builder)
+    {
+        builder.ToTable("Gifs");
+    }
+}
