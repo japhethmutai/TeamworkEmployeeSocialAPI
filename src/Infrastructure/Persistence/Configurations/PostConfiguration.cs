c6535b544d21fad575be671c487ea9246db96b28
japheth <japhemchirie@gmail.com>
2026-09-02 15:58:06 +0300

    Add PostConfiguration to enforce IsDeleted query filter from a single place for all post queries

---
+++ src/Infrastructure/Persistence/Configurations/PostConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TeamworkApp.Domain.Entities;
+
+namespace TeamworkApp.Infrastructure.Persistence.Configurations;
+
+public class PostConfiguration : IEntityTypeConfiguration<Post>
+{
+    public void Configure(EntityTypeBuilder<Post> builder)
+    {
+        builder.ToTable("Posts");
+
+        builder.HasOne(p => p.Author)
+            .WithMany(u => u.Posts)
+            .HasForeignKey(p => p.AuthorId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(p => p.Comments)
+            .WithOne(c => c.Post)
+            .HasForeignKey(c => c.PostId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasQueryFilter(p => !p.IsDeleted);
+    }
+
+}
