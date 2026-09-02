8e84775e13cde1c1a36f5fda191a9e4c2fe7aa72
japheth <japhemchirie@gmail.com>
2026-09-02 12:45:58 +0300

    Add Post and Comment entities with their properties

---
+++ src/Domain/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿namespace TeamworkApp.Domain.Entities;
+
+public class Comment
+{
+    public Guid Id { get; set; }
+    public Guid PostId { get; set; }
+    public Guid AuthorId { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsDeleted { get; set; } = false;
+    public Post Post { get; set; } = null!;
+    public User Author { get; set; } = null!;
+
+    public Comment()
+    {
+        Id = Guid.NewGuid();
+    }
+}
---
+++ src/Domain/Entities/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace TeamworkApp.Domain.Entities;
+
+public abstract class Post
+{
+    public Guid Id { get; set; }
+    public Guid AuthorId { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsDeleted { get; set; } = false;
+    public User Author { get; set; } = null!;
+    public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+    public Post()
+    {
+        Id = Guid.NewGuid();
+    }
+}
