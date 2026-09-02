5fe5236431cbba1b4d98916ba111d5d6a2b9f2f0
japheth <japhemchirie@gmail.com>
2026-09-02 12:08:29 +0300

    Add User entity with role and navigation collections

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,25 @@
+namespace TeamworkApp.Domain.Entities;
+
+public enum UserRole
+{
+    Admin,
+    Employee
+}
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public UserRole Role { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public ICollection<Post> Posts { get; set; } = new List<Post>();
+    public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+    public User()
+    {
+        Id = Guid.NewGuid();
+    }
+}
